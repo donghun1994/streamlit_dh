1d66ffdb59a01cb81c944384d20c9effeb665dde
dhko <kodh1994@naver.com>
2023-06-13 09:43:18 +0900

    prac1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+# streamlit_app.py
+
+import streamlit as st
+
+
+
+# Initialize connection.
+conn = st.experimental_connection('mysql', 'sql')
+
+# Perform query.
+df = conn.query("select * from stat_user_total_study_history where entry_date > '2023-06-10';", ttl=600)
+
 
 st.write(df)
