7aaec97083a081c451ce02f0fc89059c11afb11d
dhko <kodh1994@naver.com>
2023-06-13 10:04:41 +0900

    prac1

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import streamlit as st
 import pandas as pd
-
-
-# streamlit_app.py
-
 import streamlit as st
-
-
+from sqlalchemy import create_engine
+import sqlcon
+import mysql.connector
 
 # Initialize connection.
-conn = st.experimental_connection('mysql', 'sql')
+# conn = st.experimental_connection('mysql', 'sql')
 
 # Perform query.
-df = conn.query("select * from stat_user_total_study_history where entry_date > '2023-06-10';", ttl=600)
+# df = conn.query("select * from stat_user_total_study_history where entry_date > '2023-06-10';", ttl=600)
+engine = create_engine(sqlcon.url)
+
+
+# 쿼리 작성
+query_a = "select * from stat_user_total_study_history where entry_date > '2023-06-08' "
+df = pd.read_sql_query(query_a, con=engine)
 
 
 st.write(df)
+
---
+++ sqlcon.py
@@ -0,0 +1,1 @@
+url = 'mysql+mysqlconnector://pulleyRead:vmfl515!dnlf@pulley-cluster.cluster-ro-ce1us4oyptfa.ap-northeast-2.rds.amazonaws.com:3306/pulley'
